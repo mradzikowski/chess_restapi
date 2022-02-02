1de1988d7f08ba16e305826830a80a9c721d1243
mradzikowski <radzik.radzikowski18@gmail.com>
2022-02-02 08:56:19 +0000

    Implement get routes for checking validity of the move and checking available moves
    Added logic for checking if the chess figures passed in query, checking if the field exists. Respond with accurate response status codes. 200 - evrything ok, 409 - field does not exist, 404 - unknown chess figure

--- backend/src/api/moves/views.py
+++ backend/src/api/moves/views.py
@@ -1,15 +1,96 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
+from src.api.moves.utils import check_if_valid_chess_figure, get_figure_by_name
 
 moves_namespace = Namespace("figure")
 
+move_validity_fields = moves_namespace.model(
+    "Move Validity",
+    {
+        "figure": fields.String,
+        "move": fields.String,
+        "error": fields.String,
+        "currentField": fields.String,
+        "destField": fields.String,
+    },
+)
 
-class Move(Resource):
-    pass
+available_moves_fields = moves_namespace.model(
+    "Available Moves",
+    {
+        "availableMoves": fields.List(fields.String),
+        "error": fields.String,
+        "figure": fields.String,
+        "currentField": fields.String,
+    },
+)
 
 
-class MovesList(Resource):
-    pass
+class ValidChessMove(Resource):
+    def get(self, chess_figure: str, curr_field: str, dest_field: str):
+        response_object = {
+            "figure": chess_figure,
+            "currentField": curr_field,
+            "destField": dest_field,
+            "error": "null",
+            "move": "invalid",
+        }
+
+        if not check_if_valid_chess_figure(chess_figure=chess_figure):
+            response_object[
+                "error"
+            ] = f"Chess figure - {chess_figure} - does not exist."
+            return response_object, 404
+
+        try:
+            figure = get_figure_by_name(
+                chess_figure=chess_figure,
+                curr_field=curr_field,
+            )
+        except ValueError:
+            response_object["error"] = "Field does not exist."
+            return response_object, 409
+
+        if not figure.validate_move(dest_field=dest_field):
+            response_object["error"] = "Current move is not permitted."
+
+        response_object["move"] = "valid"
+        return response_object, 200
 
 
-moves_namespace.add_resource(Move)
-moves_namespace.add_resource(MovesList)
+class ValidChessMovesList(Resource):
+    def get(self, chess_figure: str, curr_field: str):
+        response_object = {
+            "figure": chess_figure,
+            "currentField": curr_field,
+            "error": "null",
+            "availableMoves": [],
+        }
+
+        if not check_if_valid_chess_figure(chess_figure=chess_figure):
+            response_object[
+                "error"
+            ] = f"Chess figure - {chess_figure} - does not exist."
+            return response_object, 404
+
+        try:
+            figure = get_figure_by_name(
+                chess_figure=chess_figure,
+                curr_field=curr_field,
+            )
+        except ValueError:
+            response_object["error"] = "Field does not exist."
+            return response_object, 409
+
+        available_moves = figure.list_available_moves()
+        response_object["availableMoves"] = available_moves
+        return response_object, 200
+
+
+moves_namespace.add_resource(
+    ValidChessMove,
+    "/<string:chess_figure>/<string:curr_field>/<string:dest_field>",
+)
+moves_namespace.add_resource(
+    ValidChessMovesList,
+    "/<string:chess_figure>/<string:curr_field>",
+)
